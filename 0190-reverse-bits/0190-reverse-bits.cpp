9a098c4e4bda5716a1ba4b5daaeb2bf1c3e39f11
Ajay Yadav <63529768+yadavajay2055@users.noreply.github.com>
2022-10-13 23:54:34 +0530

    Time: 7 ms (28.16%), Space: 5.9 MB (81.28%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+       if (n == 0) return 0;
+    
+    uint32_t result = 0;
+    for (int i = 0; i < 32; i++) {
+        result <<= 1;
+        if ((n & 1) == 1) result++;
+        n >>= 1;
+    }
+    return result; 
+    }
+};
